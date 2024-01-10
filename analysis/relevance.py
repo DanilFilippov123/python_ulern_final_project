b693c7cde8dfa2487f133150068a076d9c3fd20f
Филиппов Данил <filippok2004180@gmail.com>
2024-01-10 21:31:14 +0500

    Начальные графики для "Востребованности"

--- analysis/relevance.py
+++ analysis/relevance.py
@@ -6,21 +6,21 @@ def relevance(csv_file: str, count_file: str, salary_file: str):
                      parse_dates=["published_at"],
                      low_memory=False)
     df["year"] = df["published_at"].apply(lambda x: x.year)
-    df.groupby("year").count()[["name"]].to_html(f"result/{count_file}")
+    df.groupby("year").count()[["name"]].to_csv(f"result/{count_file}")
 
     df = df[~(df["salary"].isna()) & (df["salary"] < 100_000_000)]
 
     mean_salary_by_year: pd.DataFrame = df.groupby("year")["salary"].mean().reset_index()
-    mean_salary_by_year.to_html(f"result/{salary_file}", index=False, float_format=lambda x: f"{x:.3f}")
+    mean_salary_by_year.to_csv(f"result/{salary_file}", index=False, float_format=lambda x: f"{x:.3f}")
 
 
 def main():
     relevance("tmp/vacancies_our_job.csv",
-              "count_jobs_by_year_dynamic_sys_admin.html",
-              "salary_jobs_by_year_dynamic_sys_admin.html")
+              "count_jobs_by_year_dynamic_sys_admin.csv",
+              "salary_jobs_by_year_dynamic_sys_admin.csv")
     relevance("tmp/vacancies_salary_currency.csv",
-             "count_jobs_by_year_dynamic_all_jobs.html",
-             "salary_jobs_by_year_dynamic_all_jobs.html")
+             "count_jobs_by_year_dynamic_all_jobs.csv",
+             "salary_jobs_by_year_dynamic_all_jobs.csv")
 
 
 
--- analysis/relevance_graphics.py
+++ analysis/relevance_graphics.py
@@ -1,14 +1,31 @@
+import matplotlib.figure
 import pandas as pd
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 
 def main():
-    count_all_jobs = pd.read_html("result/count_jobs_by_year_dynamic_all_jobs.html")
-    count_sys_admin = pd.read_html("result/count_jobs_by_year_dynamic_sys_admin.html")
-    salary_all_jobs = pd.read_html("result/salary_jobs_by_year_dynamic_all_jobs.html")
-    salary_sys_admin = pd.read_html("result/salary_jobs_by_year_dynamic_sys_admin.html")
-    sns.lineplot(count_all_jobs, x="Год", y="Количество вакансий")
+    count_all_jobs = pd.read_csv("result/count_jobs_by_year_dynamic_all_jobs.csv",
+                                 names=["Год", "Количество вакансий"], header=0)
+    count_sys_admin = pd.read_csv("result/count_jobs_by_year_dynamic_sys_admin.csv",
+                                  names=["Год", "Количество вакансий"], header=0)
 
+    salary_all_jobs = pd.read_csv("result/salary_jobs_by_year_dynamic_all_jobs.csv",
+                                  header=0, names=["Год", "Зарплата"])
+    salary_sys_admin = pd.read_csv("result/salary_jobs_by_year_dynamic_sys_admin.csv",
+                                   header=0, names=["Год", "Зарплата"])
+
+    (sns.catplot(data=count_all_jobs, x="Год", y="Количество вакансий", kind="point", aspect=2)
+     .fig.savefig("result/count_jobs_by_year_dynamic_all_jobs.png"))
+
+    (sns.catplot(data=count_sys_admin, x="Год", y="Количество вакансий", kind="point", aspect=2)
+     .fig.savefig("result/count_jobs_by_year_dynamic_sys_admin.png"))
+
+    (sns.catplot(data=salary_all_jobs, x="Год", y="Зарплата", kind="point", aspect=2)
+     .fig.savefig("result/salary_jobs_by_year_dynamic_all_jobs.png"))
+
+    (sns.catplot(data=salary_sys_admin, x="Год", y="Зарплата", kind="point", aspect=2)
+     .fig.savefig("result/salary_jobs_by_year_dynamic_sys_admin.png"))
+    plt.show()
 
 if __name__ == "__main__":
     main()
